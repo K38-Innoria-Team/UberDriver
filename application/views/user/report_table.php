066fb270ed6170d27f11699453d3038da2ae4363
dutuannguyen <dutuannguyen94@gmail.com>
2015-06-07 02:14:53 +0700

    style_listDriver
    07062015

--- application/views/user/report_table.php
+++ application/views/user/report_table.php
@@ -1,11 +1,11 @@
 <?php
-	if(isset($_POST["btnReport"])){
-		echo "<h2>Danh sách tài xế vùng ".$_POST["txtRegion"]."</h2>";
+	/*if(isset($_POST["btnReport"])){
+		echo "<h2 style='color:white'>Danh sách tài xế vùng ".$_POST["txtRegion"]."</h2>";*/
 		//$dri = $driver;
 		//print_r($driver);
 		
 ?>
-	<table class="table table-hover">
+	<table class="table table-hover" style="background-color: white">
 	  <thead>
 	  	<th>STT</th>
 	  	<th>Họ và tên</th>
@@ -16,6 +16,29 @@
 	  	<th>Trạng thái</th>
 	  </thead>
 	  <tbody>
+      <tr>
+          <td>
+              1
+          </td>
+          <td>
+              Du Tuan Nguyen
+          </td>
+          <td>
+              0123456789
+          </td>
+          <td>
+              nguyen@mail.com
+          </td>
+          <td>
+              Uber X
+          </td>
+          <td>
+              Ho CHi Minh
+          </td>
+          <td>
+              Hoat dong
+          </td>
+      </tr>
 	  <?php
 	  	for($i=0;$i<count($driver);$i++){
 	  		?>
@@ -34,5 +57,5 @@
 	  </tbody>
 	</table>
 <?php
-	}
+	/*}*/
 ?>
